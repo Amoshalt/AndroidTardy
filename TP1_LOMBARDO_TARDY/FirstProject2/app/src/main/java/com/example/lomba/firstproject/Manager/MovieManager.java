018ac33d696de16cb6794043c1d16e5378a9314f
Martial Tardy <tardy.m02@gmail.com>
2018-09-24 15:17:24 +0200

    MovieManager for TP3

---
+++ TP1_LOMBARDO_TARDY/FirstProject2/app/src/main/java/com/example/lomba/firstproject/Manager/MovieManager.java
@@ -0,0 +1,88 @@
+package com.example.lomba.firstproject.Manager;
+
+import android.text.TextUtils;
+
+import com.example.lomba.firstproject.Model.Movie;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeSet;
+
+public class MovieManager {
+
+    private static MovieManager movieManager = null;
+
+    private Collection<Movie> movieCollection;
+
+    private MovieManager() {
+         this.movieCollection =
+                new TreeSet<Movie>(new Comparator<Movie>() {
+                    @Override
+                    public int compare(Movie movie, Movie t1) {
+                        Collator usCollator = Collator.getInstance();
+                        usCollator.setStrength(Collator.PRIMARY);
+                        int comp = usCollator.compare(movie.getmTitle(), t1.getmTitle());
+                        if(comp == 0) {
+                            int comp2 = usCollator.compare(movie.getmOriginalTitle(), t1.getmOriginalTitle());
+                            if(comp2 == 0) {
+                                return 1;
+                            } else return comp2;
+                        } else return comp;
+                    }
+                });
+         this.setMovieCollection();
+    }
+
+    public static MovieManager getInstance(){
+        if (movieManager == null) {
+            movieManager = new MovieManager();
+
+        }
+        return movieManager;
+
+    }
+
+    public List<Movie> getMovies() {
+        return new ArrayList<>(movieCollection);
+    }
+
+    public Movie getMovieById(int id) {
+        if (id <0 || id >= movieCollection.size()) {
+            return null;
+        }
+        else  {
+            ArrayList movieList = new ArrayList(movieCollection);
+            return (Movie) movieList.get(id);
+        }
+    }
+
+    private void setMovieCollection() {
+
+        for (int i = 64; i>=0; i--) {
+            movieCollection.add(new Movie("Title " + i,"Original Title " + i, "Description " + i, new String[]{"" + i, ""}, i,false));
+        }
+        movieCollection.add(new Movie("a","b","b",new String[]{"" + 1, ""}, 1,false));
+        movieCollection.add(new Movie("a","c","c",new String[]{"" + 1, ""}, 1,false));
+        movieCollection.add(new Movie("a","a","a",new String[]{"" + 1, ""}, 1,false));
+        movieCollection.add(new Movie("a","d","d",new String[]{"" + 1, ""}, 1,false));
+        movieCollection.add(new Movie("a","e","e",new String[]{"" + 1, ""}, 1,false));
+
+
+        String lastHeader = "";
+        ArrayList<Movie> movies = new ArrayList<>(movieCollection);
+        for (int i = 0; i < movieCollection.size(); i++) {
+            String header = String.valueOf(movies.get(i).getmTitle().charAt(0)).toUpperCase();
+            if (!TextUtils.equals(lastHeader, header)) {
+                lastHeader = header;
+                movies.get(i).setSection(true);
+            }
+            if (i == movieCollection.size() - 1) {
+                movies.get(i).setSection(true);
+            }
+        }
+    }
+
+}
